90e9f358a27df8a2b2cc7861ddef6f0416986f64
Dawid Duraj <dawid.duraj@prismat.de>
2024-11-27 16:00:21 +0100

    Downgrade OpenGL for mac support

--- src/engine.c
+++ src/engine.c
@@ -15,7 +15,7 @@ int engine_init(engine *engine, const char *title, int width, int height)
 
 	// Set OpenGL attributes
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 6);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 1);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 
 	// Create window
